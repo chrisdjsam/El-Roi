626e2ac1c126ce0a669244fdfd21e38dac7a0d66
Christopher Dourairaj <christopher.d.dourairaj@kp.org>
2019-02-28 17:22:07 -0800

    Modify the setup file.

--- setup.py
+++ setup.py
@@ -49,6 +49,8 @@ setup(
         'Intended Audience :: ',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ]
 )
